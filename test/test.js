31d0e96f6535b90d224afa4bdf303c423f62f986
Rebekah Whittle <whittlebekah@gmail.com>
2020-07-29 09:56:46 -0500

    ✅ test mocha

--- test/test.js
+++ test/test.js
@@ -35,7 +35,7 @@ describe('ArrayFour', function() {
 describe('ArrayFive', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
-            assert.equal([1, 2, 3].indexOf(4), 0);
+            assert.equal([1, 2, 3].indexOf(4), -1);
         });
     });
 });
