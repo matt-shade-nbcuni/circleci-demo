b9bb83118c65ae3a8485751be3704a1fe2b4714b
Rebekah Whittle <whittlebekah@gmail.com>
2020-09-01 07:05:27 -0500

    ♻️   full restructure

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import fig from "./fig.png";
+import figback from "./figback.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="wrapper">
+        <div className="wrap-container">
+          <div className="slogan">grow where you're planted.</div>
+        </div>
+        <div className="img-container">
+          <img className="image-pulse animate__animated animate__infinite	 animate__pulse" src={figback}></img>
+          <img className="image-still" src={fig}></img>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
